82d7009996fb7dd24127852f078ee4bb7626b3f6
gaelazzo <gaetano.lazzo@gmail.com>
2024-02-28 11:31:57 +0100

    fix download platform

--- downloadAdditionalFiles.js
+++ downloadAdditionalFiles.js
@@ -1,42 +1,67 @@
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
-const { promisify } = require('util');
 
-const downloadFile = promisify((url, dest, callback) => {
-  const file = fs.createWriteStream(dest);
-  https.get(url, (response) => {
-    response.pipe(file);
-    file.on('finish', () => {
-      file.close(callback);
-    });
-  }).on('error', (err) => {
-    fs.unlink(dest, () => {
-      if (callback) callback(err);
-    });
+const downloadFile = async (url, dest) => {
+  //console.log(`downloadFile: ${url} nella cartella: ${dest}`);
+  const response = await new Promise((resolve, reject) => {
+    https.get(url, resolve).on('error', reject);
   });
-});
+
+  await new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(dest);
+    response.pipe(file);
+    file.on('finish', resolve);
+    file.on('error', reject);
+  });
+
+  //console.log(`File downloaded: ${dest}`);
+};
 
 const downloadDirectory = async (url, destDir) => {
-  const response = await fetch(url);
-  const files = await response.json();
-  await Promise.all(files.map(async (file) => {
-    const filePath = path.join(destDir, file.name);
-    if (file.type === 'file') {
-      await downloadFile(file.download_url, filePath);
-    } else if (file.type === 'dir') {
-      await fs.promises.mkdir(filePath, { recursive: true });
-      await downloadDirectory(file.url, filePath);
+  try {
+    const response = await new Promise((resolve, reject) => {
+      const options = {
+        headers: {
+          'User-Agent': 'YourUserAgent' // Sostituisci 'YourUserAgent' con un'identificazione appropriata per la tua applicazione
+        }
+      };
+      https.get(url, options, resolve).on('error', reject);
+    });
+    const files = JSON.parse(await getResponseData(response));
+
+    for (const file of files) {
+      const filePath = path.join(destDir, file.name);
+
+      if (file.type === 'file') {
+        //console.log(`downloadFile il file: ${filePath}`);
+        await downloadFile(file.download_url, filePath);
+      } else if (file.type === 'dir') {
+        //console.log(`download folder: ${filePath}`);
+        await fs.promises.mkdir(filePath, { recursive: true });
+        await downloadDirectory(file.url, filePath);
+      }
     }
-  }));
+  } catch (error) {
+    console.error('Errore durante il download:', error);
+    throw error;
+  }
+};
+
+const getResponseData = async (response) => {
+  return new Promise((resolve, reject) => {
+    let data = '';
+    response.on('data', chunk => data += chunk);
+    response.on('end', () => resolve(data));
+    response.on('error', reject);
+  });
 };
 
 const platform = process.platform; // Ottieni la piattaforma corrente (darwin, win32, linux)
 const baseUrl = `https://api.github.com/repos/gaelazzo/edge-db-core/contents/lib/${platform}`;
-
 const destDir = path.join(__dirname, '../node_modules/edge-db-core/lib', platform);
-fs.mkdirSync(destDir, { recursive: true }); // Crea la directory di destinazione
 
-downloadDirectory(baseUrl, destDir)
-  .then(() => console.log('Download completato'))
-  .catch((error) => console.error('Errore durante il download:', error));
+fs.promises.mkdir(destDir, { recursive: true })
+.then(() => downloadDirectory(baseUrl, destDir))
+.then(() => console.log('Downloaded platform:'+platform))
+.catch(error => console.error('Error downloading platform ', platform+":",error));
